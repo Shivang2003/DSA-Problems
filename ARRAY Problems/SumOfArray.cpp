9a8715920533680fd5ab13b2fbf903659c26ba10
Ruthwik Kancharla <kancharlaruthwik@yahoo.in>
2022-10-09 21:40:12 +0530

    Create SumOfArray.cpp

---
+++ ARRAY Problems/SumOfArray.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std
+int main(){
+    int n,sum=0;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)cin>>a[i];
+    for(int i=0;i<n;i++)sum+=a[i];
+    cout<<sum;
+}
